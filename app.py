3dcadea9c5e8b20e04ad9e0b5c7f14f4ff0ee1da
theonlytruealex <alexiova264@gmail.com>
2025-01-14 11:42:04 +0200

    Added: ability to search for titles and links to spotify

--- app.py
+++ app.py
@@ -37,18 +37,34 @@ def get_song_album_cover_url(song, artist):
     except Exception as e:
         return "https://upload.wikimedia.org/wikipedia/commons/3/37/Sad-face.png"
 
+# Function to get song URL
+def get_spotify_url(song_title, artist_name=None):
+    query = f"{song_title} {artist_name}" if artist_name else song_title
+    try:
+        results = sp.search(q=query, type='track', limit=1)
+        if results['tracks']['items']:
+            track = results['tracks']['items'][0]
+            return track['external_urls']['spotify']
+        else:
+            return None
+    except Exception as e:
+        return None
+
 # Recommendation function
 def recommend(index):
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_music = []
     recommended_music_posters = []
+    recommended_links = []
+
     for i in distances[1:6]:
         artist = music.iloc[i[0]].artist
         song = music.iloc[i[0]].song
         recommended_music_posters.append(get_song_album_cover_url(song, artist))
         recommended_music.append(f"{song} - {artist}")
+        recommended_links.append(get_spotify_url(song, artist))
 
-    return recommended_music, recommended_music_posters
+    return recommended_music, recommended_music_posters, recommended_links
 
 # Routes
 @app.route('/')
@@ -61,11 +77,15 @@ def search():
         selected_song = request.form.get('song')
         selected_song_name = selected_song.split(" - ")[0]
         index = music[music["song"] == selected_song_name].index[0]
-        recommended_music, recommended_music_posters = recommend(index)
-        return render_template('recommendations.html', songs=zip(recommended_music, recommended_music_posters))
+        recommended_music, recommended_music_posters, recommended_links = recommend(index)
+        return render_template(
+            'recommendations.html',
+            songs=zip(recommended_music, recommended_music_posters, recommended_links)
+        )
 
     music_list = [f"{row['song']} - {row['artist']}" for index, row in music.iterrows()]
     return render_template('search.html', music_list=music_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
